8288fb5b50c1c9753eeba9c84cc2fdc5188b479b
hishamahamad <hisham.ahamad@outlook.com>
2024-04-12 22:28:44 +0300

    fix import casing issue

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/Google';
+import GoogleProvider from 'next-auth/providers/google';
 import {connectToDB} from '@utils/database';
 import User from '@models/user';
 
