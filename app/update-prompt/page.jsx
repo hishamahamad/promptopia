0d7c2b32c8652a1ee3bd349540c89bac4e492277
hishamahamad <hisham.ahamad@outlook.com>
2024-04-12 22:53:55 +0300

    add Suspense wrapper at root

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState, Suspense } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import Form from '@components/Form';
 
@@ -59,17 +59,15 @@ const EditPrompt = () => {
   }
 
   return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <Form
-            type="Edit"
-            post={post}
-            setPost={setPost}
-            submitting={submitting}
-            handleSubmit={updatePrompt}
-        >
-          Create Prompt
-        </Form>
-      </Suspense>
+      <Form
+          type="Edit"
+          post={post}
+          setPost={setPost}
+          submitting={submitting}
+          handleSubmit={updatePrompt}
+      >
+        Create Prompt
+      </Form>
   );
 };
 
